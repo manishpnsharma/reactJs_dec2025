9a28f53641ea025f5ddafb03a7a3d7e606f8f0de
manish sharma <manish.pnsharma@gmail.com>
2025-12-07 22:44:03 +0530

    assingment1

--- src/Assignment_1/data/CommentData.js
+++ src/Assignment_1/data/CommentData.js
@@ -2,27 +2,21 @@ export default {
     comments:
         [{
             "id": 1,
-            "issuedescription": "issuedescription1",
-            "severity": "Hight1",
-            "status": "status1"
+            "issuedescription": "issued  login",
+            "severity": "Minor",
+            "status": "Open"
         },
         {
             "id": 2,
-            "issuedescription": "issuedescription2",
-            "severity": "Hight2",
-            "status": "status2"
+            "issuedescription": "issuede Exception",
+            "severity": "Major",
+            "status": "Close"
         },
         {
             "id": 3,
-            "issuedescription": "issuedescription3",
-            "severity": "Hight3",
-            "status": "status3"
-        },
-        {
-            "id": 4,
-            "issuedescription": "issuedescription4",
-            "severity": "Hight4",
-            "status": "status4"
+            "issuedescription": "issuede data not display",
+            "severity": "Critical",
+            "status": "Progress"
         }
         ]
 };
